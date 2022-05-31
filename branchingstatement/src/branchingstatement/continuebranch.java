18951eaf5534af2a64ee25ec8528ab2c03c12af4
ADMIN <ADMIN@DESKTOP-V1DD98I>
2022-05-31 13:45:40 +0530

    contiune demo

---
+++ branchingstatement/src/branchingstatement/continuebranch.java
@@ -0,0 +1,12 @@
+package branchingstatement;
+
+public class continuebranch {
+	public static void main(String args[] ) {	
+for (int i=5;i<=15;i++)
+{
+	if(i%2!=0)
+		continue;
+	System.out.println(i);
+}
+}
+}
