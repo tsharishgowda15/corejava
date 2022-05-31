7da33cfccaa1ad06f661696509c32c86d1e2f019
ADMIN <ADMIN@DESKTOP-V1DD98I>
2022-05-31 13:16:26 +0530

    frist commit

---
+++ foreachloop/src/foreachloop/foreachloop.java
@@ -0,0 +1,19 @@
+package foreachloop;
+
+public class foreachloop {
+
+	public foreachloop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=5;
+while (i<=15)
+{
+	System.out.println(i);
+	i=i+2;
+}
+	}
+
+}
